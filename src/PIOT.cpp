e42a90550f28441db6cdb66593bf1feebbf648e7
Stevelee <hoang.le.tn91@gmail.com>
2024-09-15 06:13:46 +0700

    Add feature led

---
+++ src/PIOT.cpp
@@ -0,0 +1,30 @@
+#include "PIOT.h"
+#include <Arduino.h> 
+
+/**
+* We will listen data from PHP and control pin Mode 
+*/
+
+PIOT::PIOT() {
+}
+
+void PIOT::begin() {
+    pinMode(13,OUTPUT);
+}
+
+void PIOT::listen() {
+      if (Serial.available() > 0) {
+        String receivedData = Serial.readString();
+        /**
+        * Led
+        * We use pin 13 for led default 
+        */
+        if(receivedData.toInt() == 131) {
+            digitalWrite(13,HIGH);
+        } else if(receivedData.toInt() == 130) {
+            digitalWrite(13,LOW);
+        }
+        Serial.println(receivedData);
+    }
+    
+}
---
+++ src/PIOT.h
@@ -0,0 +1,13 @@
+#ifndef PIOT_H
+#define PIOT_H
+
+class PIOT {
+public:
+    PIOT();
+    void begin();
+    void listen();
+private:
+    // Các biến và hàm riêng tư nếu cần
+};
+
+#endif
