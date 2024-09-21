9c7b45846e3395c54ccd91c4ba23e43c045e824d
Stevelee <hoang.le.tn91@gmail.com>
2024-09-21 15:49:39 +0700

    Add LCD1604 and update logic core

--- src/PIOT.cpp
+++ src/PIOT.cpp
@@ -1,9 +1,15 @@
 #include "PIOT.h"
 #include <Arduino.h> 
-
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,4);
 /**
 * We will listen data from PHP and control pin Mode 
 */
+bool LCD1604INIT = false;
+int LCDRow = 0;
+int LCDCol = 0;
+
+String receivedData = "";
 
 PIOT::PIOT() {
 }
@@ -14,7 +20,23 @@ void PIOT::begin() {
 
 void PIOT::listen() {
       if (Serial.available() > 0) {
-        String receivedData = Serial.readString();
+        // String receivedData = Serial.readString();
+        receivedData += Serial.readString();
+        receivedData.replace("\n", "");
+        receivedData.replace("\r", "");
+            if (receivedData.indexOf("[END]") != -1) {
+                receivedData.replace("[END]","");
+                Serial.println(receivedData);
+                phpEvent(receivedData);
+                receivedData = "";  
+            } 
+    }
+}
+
+void PIOT::phpEvent(String receivedData) {
+    
+         String text = receivedData;
+         
         /**
         * Led
         * We use pin 13 for led default 
@@ -24,8 +46,46 @@ void PIOT::listen() {
         } else if(receivedData.toInt() == 130) {
             digitalWrite(13,LOW);
         }
-        Serial.println(receivedData);
-        
-    }
-    
+        /**
+        * LCD 1604
+        * CONNECT IC2
+        */
+        // init 
+        if(receivedData == "LCD1604INIT" && LCD1604INIT == false) {
+             LCD1604INIT = true;
+             lcd.init();                    
+             lcd.backlight(); 
+             lcd.setCursor(0,0);
+             lcd.print("Wating ....");
+        }
+
+        // control
+        if(receivedData.indexOf("A45") != -1 ) {
+                if(LCD1604INIT == true) {
+                    
+                    text.replace("A45","");
+
+                    for(int i=0;i<4;i++) {
+                            if(text.indexOf("r" + String(i)) != -1) {
+                                text.replace("r" + String(i),"");
+                                for(int e=0;e<16;e++) {
+                                if(text.indexOf("c" + String(e)) != -1) {
+                                    text.replace("c" + String(e),"");
+                                        LCDRow = i;
+                                        LCDCol = e;
+                                        lcd.setCursor(e,i);
+                                        lcd.print(text);
+                                    }
+                                }
+                        }
+                    }
+
+                    if(text.indexOf("A450") != -1) {
+                        lcd.noBacklight();
+                    } else if(text.indexOf("A451") != -1) {
+                        lcd.backlight();
+                    }
+
+            }
+        }
 }
--- src/PIOT.h
+++ src/PIOT.h
@@ -1,11 +1,12 @@
 #ifndef PIOT_H
 #define PIOT_H
-
+#include <Arduino.h> 
 class PIOT {
 public:
     PIOT();
     void begin();
     void listen();
+    void phpEvent(String receivedData);
 private:
     
 };
